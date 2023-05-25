1bf72bd9cc13469b24756ebeee9f7d2ae709b7db
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2023-05-25 10:21:42 -0300

    Finish table statistic

---
+++ src/components/TableInfo.tsx
@@ -0,0 +1,94 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  VStack,
+  Text,
+} from '@chakra-ui/react'
+
+interface IPlayer {
+  name: string,
+  age: number,
+  nationality: string,
+}
+
+interface IPlayers {
+  players: IPlayer[]
+}
+
+interface IStatistic {
+  games: string,
+  wins: string,
+  draws: string,
+  loses: string,
+}
+
+export const TablePlayer = ({ players }: IPlayers) => (
+  <VStack
+    width="80%"
+    height="600px"
+    alignItems="center"
+  >
+    <Text fontSize="2xl" fontFamily="'Inter Variable', sans-serif" fontWeight="medium">
+      Players
+    </Text>
+    <TableContainer height="full" width="50%" overflowY="auto" overflowX="hidden">
+      <Table variant='striped' colorScheme="blackAlpha">
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th >Year</Th>
+            <Th isNumeric>Nationality</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {
+            players.map((player, index) => (
+              <Tr key={index}>
+                <Td>{player.name}</Td>
+                <Td >{player.age}</Td>
+                <Td isNumeric>{player.nationality}</Td>
+              </Tr>
+            ))
+          }
+        </Tbody>
+      </Table>
+    </TableContainer>
+  </VStack>
+)
+
+export const TableTeamStatistic = (props: IStatistic) => (
+  <VStack
+    width="80%"
+    height="600px"
+    alignItems="center"
+  >
+    <Text fontSize="2xl" fontFamily="'Inter Variable', sans-serif" fontWeight="medium">
+      Statistic
+    </Text>
+    <TableContainer width="50%" >
+      <Table variant='simple'>
+        <Thead>
+          <Tr>
+            <Th>Games</Th>
+            <Th>Win</Th>
+            <Th>Drawns</Th>
+            <Th>Loses</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>{props.games}</Td>
+            <Td>{props.wins}</Td>
+            <Td>{props.draws}</Td>
+            <Td>{props.loses}</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
+  </VStack>
+)
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,19 +1,9 @@
 import { Header } from "../components/Header"
 import { useLocation } from "react-router-dom";
 import { SelectGroup } from "../components/SelectGroup";
-import { HStack, VStack } from "@chakra-ui/react";
+import { HStack, Text, Center, VStack } from "@chakra-ui/react";
 import { useState } from "react";
-
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  Text,
-  TableContainer,
-} from '@chakra-ui/react'
+import { TablePlayer, TableTeamStatistic } from "../components/TableInfo";
 
 function Dashboard() {
   const { state } = useLocation();
@@ -25,17 +15,17 @@ function Dashboard() {
     league: number
   }
 
-  interface Player {
+  interface IPlayer {
     name: string,
     age: number,
     nationality: string,
   }
 
-  interface Players {
-    players: Player[]
+  interface IPlayers {
+    players: IPlayer[]
   }
 
-  interface Statistic {
+  interface IStatistic {
     games: string,
     wins: string,
     draws: string,
@@ -44,9 +34,9 @@ function Dashboard() {
   }
 
   const [team, setTeam] = useState<TeamNumber>({ team: 0, season: 0, league: 0 })
-  const [statistic, setStatistic] = useState<Statistic>({ games: "", wins: "", draws: "", loses: "", lineups: "" })
-  
-  const [players, setPlayers] = useState<Players>({ players: [] })
+  const [statistic, setStatistic] = useState<IStatistic>({ games: "", wins: "", draws: "", loses: "", lineups: "" })
+
+  const [players, setPlayers] = useState<IPlayers>({ players: [] })
 
   return (
     <VStack spacing="2%">
@@ -59,70 +49,24 @@ function Dashboard() {
       />
 
       <Text fontSize="2xl" fontFamily="'Inter Variable', sans-serif" fontWeight="medium">
-        Most used tactic: {statistic.lineups}
+        Most used tactic: <Text as="span" color="green.500">{statistic.lineups}</Text>
       </Text>
 
-      <HStack justifyContent="space-between" minW="70%" justifyItems="flex-start" alignItems="flex-start">
-        <VStack alignItems="flex-start">
-          <Text
-            fontSize="2xl"
-            fontFamily="'Inter Variable', sans-serif"
-            fontWeight="medium"
-          >
-            Player List
-          </Text>
-          <TableContainer maxH="2%" >
-            <Table variant='striped' colorScheme='dark'>
-              <Thead>
-                <Tr>
-                  <Th>Name</Th>
-                  <Th>Age</Th>
-                  <Th>Nationality</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {
-                  players.players.map((player, index) => (
-                    <Tr key={index}>
-                      <Td>{player.name}</Td>
-                      <Td>{player.age}</Td>
-                      <Td>{player.nationality}</Td>
-                    </Tr>
-                  ))
-                }
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </VStack>
-        <VStack alignItems="flex-start" >
-          <Text
-            fontSize="2xl"
-            fontFamily="'Inter Variable', sans-serif"
-            fontWeight="medium"
-          >
-            Team Statistic
-          </Text>
-          <TableContainer>
-            <Table variant='striped' colorScheme='dark'>
-              <Thead>
-                <Tr>
-                  <Th>Games</Th>
-                  <Th>Wins</Th>
-                  <Th>Draws</Th>
-                  <Th>Loses</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td>{statistic.games}</Td>
-                  <Td>{statistic.wins}</Td>
-                  <Td>{statistic.draws}</Td>
-                  <Td>{statistic.loses}</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </VStack>
+      <HStack
+        alignItems="flex-start"
+        justifyContent="space-between"
+        height="600px"
+        width="80%"
+        p={2}
+        spacing={4}
+      >
+        <TablePlayer players={players.players} />
+        <TableTeamStatistic
+          games={statistic.games}
+          wins={statistic.wins}
+          draws={statistic.draws}
+          loses={statistic.loses}
+        />
       </HStack>
 
     </VStack>
